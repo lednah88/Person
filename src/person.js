2457376764aeb76f53f324304efdfbbe9f0c72f0
Rixongile Manyobe <lednah97@gmail.com>
2020-05-05 13:22:18 +0200

    console.log(greeting) uncommented

--- src/person.js
+++ src/person.js
@@ -1,16 +1,20 @@
 class Person {
     constructor(name, age, gender, interest) {
-        this.name = name
-        this.age = age
-        this.gender = gender
-        this.interest = interest
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.interest = interest;
     }
     hello() {
-        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old. My interests are ${this.interest[0]},${this.interest[1]} and ${this.interest[2]}.`)
+        return `Hello, my name is ${this.name} and I am ${this.age} years old. My interests are ${this.interest[0]},${this.interest[1]} and ${this.interest[2]}.`;
     }
 }
-let person = new Person('Ryan', 30, 'male', [' being a hardarse', ' agile', ' ssd hard drives'])
-let greeting = person.hello()
-    // console.log(greeting)
+let person = new Person("Ryan", 30, "male", [
+    " being a hardarse",
+    " agile",
+    " ssd hard drives",
+]);
+let greeting = person.hello();
+console.log(greeting);
 
-module.exports = { person }
+module.exports = { Person };
